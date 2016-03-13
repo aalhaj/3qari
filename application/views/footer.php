97ac9d5e88b379dd0a70133fab9d98f3d0de7df4
aalhaj <eng.ahmad753@gmail.com>
2016-03-13 06:52:46 +0200

    modifications

---
+++ application/views/footer.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+			</div> <!-- end of container -->
+		</div> <!-- end of wrapper -->
+	</body>
+</html>
+
+<!-- Script -->
+
+<script type="text/javascript" src="<?php echo base_url() . "application/assets/js/jquery-1.10.2.js"; ?>"></script><!-- Jquery -->
+<script type="text/javascript" src="<?php echo base_url() . "application/assets/js/d3.v2.js"; ?>"></script><!-- VISITOR CHART -->
+<script type="text/javascript" src="<?php echo base_url() . "application/assets/js/nv.d3.js"; ?>"></script><!-- VISITOR CHART -->
+<script type="text/javascript" src="<?php echo base_url() . "application/assets/js/bootstrap.min.js"; ?>"></script><!-- Bootstrap -->
+<script type="text/javascript" src="<?php echo base_url() . "application/assets/js/script.js"; ?>"></script><!-- Script -->
+<script type="text/javascript" src="<?php echo base_url() . "application/assets/js/skycons.js"; ?>"></script> <!-- Skycons -->
+<script type="text/javascript" src="<?php echo base_url() . "application/assets/js/enscroll-0.5.2.min.js"; ?>"></script> <!-- Custom Scroll bar -->
